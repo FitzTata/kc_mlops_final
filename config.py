22d742017ca51a802b23df539f6c2d52dcf5944b
Fitz_Tata <fitz163@gmail.com>
2024-11-25 20:22:43 +0300

    fix reload model

--- config.py
+++ config.py
@@ -6,5 +6,5 @@ PROD_ALIAS = "prod"
 REGISTERED_MODEL_NAME = "a-davydov-project-model"
 GITLAB_PROJECT_NAME = "aleksandr-davydov-wtf9954/course-project"
 
-os.environ["MLFLOW_TRACKING_URI"] = "https://lab-mlflow.karpov.courses/#"
+os.environ["MLFLOW_TRACKING_URI"] = "https://lab-mlflow.karpov.courses"
 os.environ["AWS_ENDPOINT_URL"] = "https://storage.yandexcloud.net"
