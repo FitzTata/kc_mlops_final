b9879b5deaccfed98fe3488e9ff53eba1505f7d9
Fitz_Tata <fitz163@gmail.com>
2024-11-25 21:49:21 +0300

    fix reload model

--- config.py
+++ config.py
@@ -3,7 +3,7 @@
 import os
 
 PROD_ALIAS = "prod"
-REGISTERED_MODEL_NAME = "a-davydov-project-model"
+REGISTERED_MODEL_NAME = "aleksandr-davydov-wtf9954-project-model"
 GITLAB_PROJECT_NAME = "aleksandr-davydov-wtf9954/course-project"
 
 os.environ["MLFLOW_TRACKING_URI"] = "https://lab-mlflow.karpov.courses"
